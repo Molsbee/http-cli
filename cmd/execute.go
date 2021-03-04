5933b821c2c367ce0e6b30516aa242e8b9503ed3
molsbee <molsbee@gmail.com>
2021-03-04 11:58:12 -0600

    Added feature for execute to parse response and then inject the values back into the original yaml file

--- cmd/execute.go
+++ cmd/execute.go
@@ -1,14 +1,17 @@
 package cmd
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/Molsbee/http-cli/service"
+	"github.com/savaki/jq"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os"
 	"strings"
+	"text/template"
 )
 
 type requests struct {
@@ -16,11 +19,12 @@ type requests struct {
 }
 
 type request struct {
-	Name    string   `json:"name"`
-	Method  string   `json:"method"`
-	URL     string   `json:"url"`
-	Headers []string `json:"headers"`
-	Data    string   `json:"data"`
+	Name    string            `json:"name"`
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Headers []string          `json:"headers"`
+	Data    string            `json:"data"`
+	Parse   map[string]string `json:"parse"`
 }
 
 var executeCmd = &cobra.Command{
@@ -33,43 +37,66 @@ var executeCmd = &cobra.Command{
 		}
 
 		filePath := args[0]
-		bytes, err := ioutil.ReadFile(filePath)
+		b, err := ioutil.ReadFile(filePath)
 		if err != nil {
 			fmt.Printf("failed to read file (%s) - %s", filePath, err)
 			os.Exit(1)
 		}
 
 		var requests requests
-		if err := yaml.Unmarshal(bytes, &requests); err != nil {
+		if err := yaml.Unmarshal(b, &requests); err != nil {
 			fmt.Printf("failed to convert file to expected format - %s", err)
 			os.Exit(1)
 		}
 
+		variables := make(map[string]string)
 		client := service.NewRestClient(true, true)
-		for _, request := range requests.Requests {
+		for i := 0; i < len(requests.Requests); i++ {
+			request := requests.Requests[i]
 			fmt.Printf("##### %s #####", request.Name)
-
-			var resp string
-			var clientErr error
-			switch {
-			case strings.EqualFold(request.Method, "GET"):
-				resp, clientErr = client.Get(request.URL, parseHeaders(request.Headers))
-			case strings.EqualFold(request.Method, "PUT"):
-				resp, clientErr = client.Put(request.URL, request.Data, parseHeaders(request.Headers))
-			case strings.EqualFold(request.Method, "POST"):
-				resp, clientErr = client.Post(request.URL, request.Data, parseHeaders(request.Headers))
-			case strings.EqualFold(request.Method, "DELETE"):
-				resp, clientErr = client.Delete(request.URL, request.Data, parseHeaders(request.Headers))
-			default:
-				resp, clientErr = "unsupported method", errors.New("unsupported method")
-			}
-
+			resp, clientErr := executeRequest(client, request)
 			if clientErr != nil {
 				fmt.Println(clientErr)
-			} else {
-				fmt.Println(resp)
+				fmt.Println("##### END OF REQUEST #####")
+				continue
 			}
+
+			if updated := updateVariables(variables, request, resp); updated {
+				var buffer bytes.Buffer
+				temp, _ := template.ParseFiles(filePath)
+				temp.Execute(&buffer, variables)
+
+				yaml.Unmarshal(buffer.Bytes(), &requests)
+			}
+
+			fmt.Println(resp)
 			fmt.Println("##### END OF REQUEST #####")
 		}
 	},
+}
+
+func executeRequest(client service.RestClient, request request) (string, error) {
+	switch {
+	case strings.EqualFold(request.Method, "GET"):
+		return client.Get(request.URL, parseHeaders(request.Headers))
+	case strings.EqualFold(request.Method, "PUT"):
+		return client.Put(request.URL, request.Data, parseHeaders(request.Headers))
+	case strings.EqualFold(request.Method, "POST"):
+		return client.Post(request.URL, request.Data, parseHeaders(request.Headers))
+	case strings.EqualFold(request.Method, "DELETE"):
+		return client.Delete(request.URL, request.Data, parseHeaders(request.Headers))
+	}
+
+	return "unsupported method", errors.New("unsupported method")
+}
+
+func updateVariables(variables map[string]string, request request, resp string) bool {
+	for k, v := range request.Parse {
+		op, _ := jq.Parse(v)
+		value, _ := op.Apply([]byte(resp))
+		variables[k] = strings.TrimSuffix(strings.TrimPrefix(string(value), "\""), "\"")
+		return true
+	}
+
+	return false
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Molsbee/http-cli
 go 1.15
 
 require (
+	github.com/savaki/jq v0.0.0-20161209013833-0e6baecebbf8
 	github.com/spf13/cobra v1.1.3
 	github.com/yosssi/gohtml v0.0.0-20201013000340-ee4748c638f4
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
